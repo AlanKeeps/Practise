5fdcd94247e06ef04b6cecad1a99f0ad8c0c0a01
AlanKeeps <alan.keeps.walking@gmail.com>
2022-10-17 15:48:20 +0300

    change mappers and models

--- app/src/main/java/com/example/cryptoapp/data/database/CoinInfoDbModel.kt
+++ app/src/main/java/com/example/cryptoapp/data/database/CoinInfoDbModel.kt
@@ -6,12 +6,12 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "full_price_list")
 data class CoinInfoDbModel(
     @PrimaryKey
-    val fromSymbol: String?,
+    val fromSymbol: String,
     val toSymbol: String?,
     val price: String?,
     val lastUpdate: Long?,
     val highDay: String?,
     val lowDay: String?,
     val lastMarket: String?,
-    val imageUrl: String?
+    val imageUrl: String
 )
